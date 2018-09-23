7b38a57e7ae61d164ed736055cc5fdc29b98b02f
Joshpitzalis <joshpitzalis@gmail.com>
2018-09-23 14:25:41 +0530

    now you can get non auction gems

--- src/pages/dashboard/dashboardActions.js
+++ src/pages/dashboard/dashboardActions.js
@@ -1,19 +1,32 @@
-import { USER_GEMS_RETRIEVED} from './dashboardConstants'
-import {db} from '../../utils/firebase'
-import store from '../../store'
+import { USER_GEMS_RETRIEVED, ALL_USER_GEMS_RETRIEVED } from "./dashboardConstants";
+import { db } from "../../utils/firebase";
+import store from "../../store";
 
-export const getUserGems =  (userId) => () => db
-.collection('auctions')
-.where('owner', '==', userId)
-.get()
-.then(collection => {
-   const gems = collection.docs.map(doc => doc.data())
-   store.dispatch({type:USER_GEMS_RETRIEVED, payload: gems})
-})
+// const getAllUserGems = (userId, gemsContract) => {
+const getAllUserGems = () => {
+  const userId = store.getState().auth.currentUserId;
+  const gemContract = store.getState().app.gemsContractInstance;
 
+  gemContract.methods
+    .getCollection(userId)
+    .call()
+    .then(payload =>
+      store.dispatch({
+        type: ALL_USER_GEMS_RETRIEVED,
+        payload
+      })
+    );
+};
 
+export const getUserGems = userId => () =>
+  db
+    .collection("auctions")
+    .where("owner", "==", userId)
+    .get()
+    .then(collection => {
+      const gems = collection.docs.map(doc => doc.data());
+      store.dispatch({ type: USER_GEMS_RETRIEVED, payload: gems });
+      getAllUserGems();
+    });
 
-
-
-
-export const TEMP = () => console.log('frog')
+export const TEMP = () => console.log("frog");
