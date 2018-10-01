42f3aa2ec1c3a845d80202157a26172841561440
Joshpitzalis <joshpitzalis@gmail.com>
2018-10-01 10:27:33 +0530

    cra2 integrated

---
+++ build/precache-manifest.434e5f721bc5417ed952bf206d4c8a10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b53a941b4cf9d208f693a32fa434353",
+    "url": "/static/media/footer_geodes.8b53a941.png"
+  },
+  {
+    "revision": "b146313364678313898a",
+    "url": "/static/css/main.5df4e8fb.chunk.css"
+  },
+  {
+    "revision": "7f69621e9f4f5b5c7b6ea88b99cd2fcc",
+    "url": "/static/media/muli-v11-latin-900.7f69621e.woff2"
+  },
+  {
+    "revision": "ee05f1d5db0eadf1fdb0",
+    "url": "/static/js/1.ee05f1d5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca7bfdfb6dfd1aedfa1fe8d2c24e9b46",
+    "url": "/static/media/rockBackground.ca7bfdfb.png"
+  },
+  {
+    "revision": "970bf8d75d1138d9cfd8fe9f5d898201",
+    "url": "/static/media/gemKid.970bf8d7.png"
+  },
+  {
+    "revision": "b146313364678313898a",
+    "url": "/static/js/main.b1463133.chunk.js"
+  },
+  {
+    "revision": "c4c1ace647ba931bd8e97f06bd09fbe2",
+    "url": "/static/media/pinkBuyNowButton.c4c1ace6.png"
+  },
+  {
+    "revision": "45e555f9d7a99c5b06bf70196c20abbe",
+    "url": "/static/media/muli-v11-latin-regular.45e555f9.woff2"
+  },
+  {
+    "revision": "e40161a93339af51847eee49c683f08f",
+    "url": "/static/media/muli-v11-latin-regular.e40161a9.woff"
+  },
+  {
+    "revision": "3f016a1fd0a668cfb421ddafb044ba30",
+    "url": "/static/media/muli-v11-latin-900.3f016a1f.woff"
+  },
+  {
+    "revision": "ee05f1d5db0eadf1fdb0",
+    "url": "/static/css/1.d64342aa.chunk.css"
+  },
+  {
+    "revision": "2db53b144bb8f5c42bfface96a947048",
+    "url": "/index.html"
+  }
+];
