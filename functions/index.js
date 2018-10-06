fe16587b105fc5523c63bfaa8e7d2ef66fff5d21
Joshpitzalis <joshpitzalis@gmail.com>
2018-10-06 18:19:49 +0530

    had to add checksum to compare ethereum addresses

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+const AUCTION_CONTRACT = '0x4ec415d87e00101867fbfa28db19cce0d564d8b3';
+const GEM_CONTRACT = '0x82ff6bbd7b64f707e704034907d582c7b6e09d97';
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 var Web3 = require('web3');
@@ -18,31 +20,51 @@ const gemsABI = Gems.abi;
 
 // @notice instantiating gem contract
 const gemsContract = new web3.eth.Contract(gemsABI);
-gemsContract.options.address = '0x82ff6bbd7b64f707e704034907d582c7b6e09d97';
+gemsContract.options.address = GEM_CONTRACT;
 
-exports.updateGemDetails = functions.https.onRequest(
-  () =>
-    admin
-      .firestore()
-      .collection('stones')
-      .get()
-      .then(coll => {
-        const docs = coll.docs.map(doc => [doc.id, doc.data()]);
-        return docs;
-      })
-      .then(docs =>
-        Promise.all(
-          docs.map(doc =>
-            gemsContract.methods
-              .ownerOf(doc[1].id)
-              .call()
-              .then(address => [doc[0], address])
-          )
+exports.updateGemDetails = functions.https.onRequest(() =>
+  admin
+    .firestore()
+    .collection('stones')
+    .get()
+    .then(coll => {
+      const docs = coll.docs.map(doc => [doc.id, doc.data()]);
+      return docs;
+    })
+    .then(docs =>
+      Promise.all(
+        docs.map(doc =>
+          gemsContract.methods
+            .ownerOf(doc[1].id)
+            .call()
+            .then(address => [doc[0], address])
         )
       )
-      .then(addresses => {
-        // maybe in the future get image and name from db and update that too
-        return addresses.forEach(address =>
+    )
+    .then(addresses => {
+      // maybe in the future get image and name from db and update that too
+      return addresses.forEach(address => {
+        console.log(
+          'gem',
+          address[0],
+          'gem owner',
+          address[1],
+          web3.utils.toChecksumAddress(address[1]),
+          web3.utils.toChecksumAddress(address[1]) ===
+            web3.utils.toChecksumAddress(AUCTION_CONTRACT)
+        );
+        if (
+          web3.utils.toChecksumAddress(address[1]) ===
+          web3.utils.toChecksumAddress(AUCTION_CONTRACT)
+        ) {
+          admin
+            .firestore()
+            .collection('stones')
+            .doc(address[0])
+            .update({
+              auctionIsLive: true
+            });
+        } else {
           admin
             .firestore()
             .collection('stones')
@@ -50,12 +72,9 @@ exports.updateGemDetails = functions.https.onRequest(
             .update({
               owner: address[1],
               auctionIsLive: false
-            })
-        );
-
-        // how do I get it to stop timing out here? Is there a done()?
-      })
-      .catch(err => console.log('err', err))
-
-  // maybe run another function agianst the auction contract to confim projects are in auction
+            });
+        }
+      });
+    })
+    .catch(err => console.log('err', err))
 );
