b0cd6ef944dec7e7c4c3bcdfb4444a0375b974ea
Joshpitzalis <joshpitzalis@gmail.com>
2018-11-12 13:38:23 +0530

    country dashboard reveal

--- src/features/countries/components/Dashboard/CountryDisplay.jsx
+++ src/features/countries/components/Dashboard/CountryDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Transition } from 'react-spring';
 import CountryDetails from './CountryDetails';
 import { CountryBar } from './CountryBar';
 import { handleResell } from '../../helpers';
@@ -8,6 +9,80 @@ import { handleResell } from '../../helpers';
 require('antd/lib/avatar/style/css');
 require('antd/lib/icon/style/css');
 require('antd/lib/card/style/css');
+
+const smoothScroll = {
+  timer: null,
+  stop() {
+    clearTimeout(this.timer);
+  },
+
+  scrollTo(id, callback) {
+    const settings = {
+      duration: 2000,
+      easing: {
+        outQuint(x, t, b, c, d) {
+          // eslint-disable-next-line
+          return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+        },
+      },
+    };
+    let percentage;
+    let startTime;
+    const node = document.getElementById(id);
+    const nodeTop = node.offsetTop;
+    const nodeHeight = node.offsetHeight;
+    // eslint-disable-next-line
+    const body = document.body;
+    const html = document.documentElement;
+    const height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight,
+    );
+    const windowHeight = window.innerHeight;
+    const offset = window.pageYOffset;
+    const delta = nodeTop - offset;
+    const bottomScrollableY = height - windowHeight;
+    const targetY = bottomScrollableY < delta
+      ? bottomScrollableY - (height - nodeTop - nodeHeight + offset)
+      : delta;
+
+    // eslint-disable-next-line
+    startTime = Date.now();
+    percentage = 0;
+
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+
+    function step() {
+      let yScroll;
+      const elapsed = Date.now() - startTime;
+
+      if (elapsed > settings.duration) {
+        clearTimeout(this.timer);
+      }
+
+      percentage = elapsed / settings.duration;
+
+      if (percentage > 1) {
+        clearTimeout(this.timer);
+
+        if (callback) {
+          callback();
+        }
+      } else {
+        yScroll = settings.easing.outQuint(0, elapsed, offset, targetY, settings.duration);
+        window.scrollTo(0, yScroll);
+        this.timer = setTimeout(step, 10);
+      }
+    }
+
+    this.timer = setTimeout(step, 10);
+  },
+};
 
 class Countries extends Component {
   static propTypes = {
@@ -23,41 +98,55 @@ class Countries extends Component {
 
   state = {
     index: 0,
+    selected: false,
   };
 
   selectCountry = (index) => {
-    this.setState({ index });
+    smoothScroll.scrollTo('top');
+    this.setState({ index, selected: true });
+    // window.scrollTo(0, 0);
   };
 
   render() {
-    const { index } = this.state;
+    const { index, selected } = this.state;
     const {
       countries, CountryERC721, DutchContract, userId,
     } = this.props;
 
-    console.log('countries', countries);
     return (
-      <div data-testid="countriesExist">
-        <CountryDetails
-          name={countries[index].name}
-          lastBought={countries[index].lastBought}
-          description={countries[index].description}
-          totalPlots={countries[index].totalPlots}
-          plotsBought={countries[index].plotsBought}
-          plotsMined={countries[index].plotsMined}
-          plotsAvailable={countries[index].plotsAvailable}
-          image={countries[index].image}
-          lastPrice={countries[index].lastPrice}
-          roi={countries[index].roi}
-          handleResell={handleResell}
-          sellMethod={CountryERC721 && CountryERC721.methods}
-          countrySaleContractId={process.env.REACT_APP_DUTCH_AUCTION}
-          userId={userId}
-          countryId={countries[index].countryId}
-          onSale={countries[index].onSale}
-          erc721CountryContract={process.env.REACT_APP_COUNTRY_ERC721}
-          dutchContractMethods={DutchContract && DutchContract.methods}
-        />
+      <div data-testid="countriesExist" id="top">
+        <Transition
+          items={countries[index]}
+          from={{ transform: 'translate3d(0,-40px,0)' }}
+          enter={{ transform: 'translate3d(0,0px,0)' }}
+          leave={{ transform: 'translate3d(0,-40px,0)' }}
+        >
+          {item => selected
+            && item && (
+              <CountryDetails
+                name={countries[index].name}
+                lastBought={countries[index].lastBought}
+                description={countries[index].description}
+                totalPlots={countries[index].totalPlots}
+                plotsBought={countries[index].plotsBought}
+                plotsMined={countries[index].plotsMined}
+                plotsAvailable={countries[index].plotsAvailable}
+                image={countries[index].image}
+                lastPrice={countries[index].lastPrice}
+                roi={countries[index].roi}
+                handleResell={handleResell}
+                sellMethod={CountryERC721 && CountryERC721.methods}
+                countrySaleContractId={process.env.REACT_APP_DUTCH_AUCTION}
+                userId={userId}
+                countryId={countries[index].countryId}
+                onSale={countries[index].onSale}
+                erc721CountryContract={process.env.REACT_APP_COUNTRY_ERC721}
+                dutchContractMethods={DutchContract && DutchContract.methods}
+              />
+          )
+          }
+        </Transition>
+
         <CountryBar countries={countries} selectCountry={this.selectCountry} />
       </div>
     );
--- src/features/countries/index.jsx
+++ src/features/countries/index.jsx
@@ -8,13 +8,13 @@ import geoData from '../../app/maps/world-50m-with-population.json';
 import DetailsBar from './components/DetailsBar';
 
 const cities = [
-  { name: 'Zurich', coordinates: [8.5417, 47.3769] },
+  { name: 'Switzerland', coordinates: [8.5417, 47.3769] },
   { name: 'Singapore', coordinates: [103.8198, 1.3521] },
-  { name: 'San Francisco', coordinates: [-122.4194, 37.7749] },
-  { name: 'Sydney', coordinates: [151.2093, -33.8688] },
-  { name: 'Lagos', coordinates: [3.3792, 6.5244] },
-  { name: 'Buenos Aires', coordinates: [-58.3816, -34.6037] },
-  { name: 'Shanghai', coordinates: [121.4737, 31.2304] },
+  { name: 'United States', coordinates: [-122.4194, 37.7749] },
+  { name: 'Australia', coordinates: [151.2093, -33.8688] },
+  { name: 'Nigeria', coordinates: [3.3792, 6.5244] },
+  { name: 'Argentina', coordinates: [-58.3816, -34.6037] },
+  { name: 'China', coordinates: [121.4737, 31.2304] },
 ];
 
 const CountryAuction = () => {
