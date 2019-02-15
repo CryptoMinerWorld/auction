4156b0b26cae8cd4dd203678cb8d92668042d4af
Sophophilia Studios <sophophiliastudios@gmail.com>
2019-02-14 19:11:07 -0500

    Revert "Test with Grad Change"
    This reverts commit 24c6f50463148fe486fab0d127e2d4404c5b3d87.

--- src/features/mint/Mint.jsx
+++ src/features/mint/Mint.jsx
@@ -74,7 +74,7 @@ class Mint extends PureComponent {
       3: 'B',
       4: 'A',
       5: 'AA',
-      6: 'BBB',
+      6: 'AAA',
     }[gradeType];
     console.log('FUCKFUCKFUCKFUCKFUCK');
     const fileName = `${type}-${level}-${grade}-4500.png`;
